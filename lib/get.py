3193165a308e735ce322a275ca5afc5e9f6c1b48
Ochuodho <ochuodhoderrick@gmail.com>
2023-10-20 16:56:54 +0300

    passed

--- lib/get.py
+++ lib/get.py
@@ -3,8 +3,10 @@ import json
 
 url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/locations.json"
 
-response = requests.get(url)
+response =  requests.get(url)
 
 json_content = json.loads(response.text)
 
 print(json.dumps(json_content, indent=4))
+
+
